d8aac243697e6959d6955b50134184d3083aebbd
Luan Ferreira Benevides <Luan_benevides_ti@outlook.com>
2023-08-07 20:30:09 -0300

    Project upload

--- src/main/java/br/com/modelar/sislogmobile/SisSecurityApplication.java
+++ src/main/java/br/com/modelar/sislogmobile/SisSecurityApplication.java
@@ -2,16 +2,15 @@ package br.com.modelar.sislogmobile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class SislogMobileApplication {
+public class SisSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SislogMobileApplication.class, args);
+		SpringApplication.run(SisSecurityApplication.class, args);
 	}
 
 	@Bean
